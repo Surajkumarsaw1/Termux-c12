573fb0bc586a1171570a5a5e4a60130c6af1b193
Surajkumarsaw1 <44591689+Surajkumarsaw1@users.noreply.github.com>
2020-11-30 21:39:37 +0530

    Update termux_installer.sh

--- termux_installer.sh
+++ termux_installer.sh
@@ -1,58 +1,113 @@
-apt update -y
-apt upgrade -y
-termux-setup-storage
+#!/bin/bash
 
-apt install clang -y
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
 
-apt install git -y
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m Suraj"
+figlet -f big  Please be patience | lolcat
 
-apt install python -y
+echo "updating and upgrading";
+apt update
+apt upgrade
 
-apt install python-dev -y
+echo "installing clang";
+apt install clang
 
-apt install fftw -y
+echo "installing git";
+apt install git
 
-apt install libzmq -y
+echo "intalling python";
+apt install python
 
-apt install libzmq-dev -y
+echo "installing fftw";
+apt install fftw
 
-apt install freetype -y
+echo "installing pkg-config";
+apt install pkg-config
 
-apt install freetype-dev -y
-
-apt install libpng -y
-
-apt install libpng-dev -y
-
-apt install pkg-config -y
-
+echo "upgrading pip";
 pip install --upgrade pip
 
+echo "intalling wheel";
 pip install wheel
 
-LDFLAGS=" -lm -lcompiler_rt" pip install numpy
+echo "installing requirements for gmpy2";
+apt install libgmp libmpc libmpfr
 
-apt install zlib zlib-dev
+echo "installing requirements for lxml";
+apt install libxml2 libxslt 
 
-LDFLAGS=" -lm -lcompiler_rt" pip install matplotlib
+echo "installing requirements for matplotlib";
+apt install freetype libpng 
 
+echo "installing requirements for pynacl";
+apt install libsodium 
+
+echo "installing requirements for pillow";
+apt install libjpeg-turbo libpng 
+
+echo "installing requirements for pyzmq";
+apt install libzmq 
+
+echo "subscribing to APT repository";
+curl -LO https://its-pointless.github.io/setup-pointless-repo.sh
+bash setup-pointless-repo.sh
+
+echo "installing package of numpy and scipy";
+pkg install numpy
+pkg install scipy
+
+pip install numpy
+pip install scipy
+
+echo "installing dependences for opencv";
+pkg install build-essential cmake libjpeg-turbo libpng python
+
+echo "installing pandas";
+export CFLAGS="-Wno-deprecated-declarations -Wno-unreachable-code"
+pip install pandas
 LDFLAGS=" -lm -lcompiler_rt" pip install pandas
 
-LDFLAGS=" -lm -lcompiler_rt" pip install jupyter
+echo "installing zlib";
+apt install zlib
 
-apt install wget -y
+echo "installing matplotlib"
+CFLAGS=" -I/data/data/com.termux/files/usr/include/freetype2" CPPFLAGS=$CFLAGS LDFLAGS=" -lm -lcompiler_rt" pip install matplotlib
 
-$PREFIX/bin/wget https://its-pointless.github.io/setup-pointless-repo.sh 
-bash setup-pointless-repo.sh
-apt install scipy -y
+echo "installing jupyter notebook";
+pip install jupyterlab
 
+echo "installing wget";
+apt install wget
+
+echo "installing cython";
 pip install cython
+
+echo "installing scikit-learn"
 LDFLAGS=" -lm -lcompiler_rt" pip install scikit-learn
 
-apt install opencv -y
+echo "installing opencv"
+apt install opencv
+pip isntall opencv
 
-pip install jupyter
-pip install numpy
-pip install matplotlib
-pip install pandas
-pip install scikit-learn
+LDFLAGS="-L/system/lib/" CFLAGS=" -I/data/data/com.termux/files/usr/include/" pip install Pillow
+
+CFLAGS=" -I/data/data/com.termux/files/usr/include/freetype2" CPPFLAGS=$CFLAGS LDFLAGS=" -lm -lcompiler_rt" pip install numpy matplotlib pandas jupyter
+
+LDFLAGS=" -lm -lcompiler_rt" pip install numpy matplotlib pandas jupyter
+
+pip install numpy matplotlib pandas jupyter
+
+figlet -f big  Congratulation Done  !!! | lolcat
+figlet -f big  Please check that all package were installed correctly | lolcat
+
+pip list | lolcat
